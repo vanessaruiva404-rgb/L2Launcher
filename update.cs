dd962ef9f554952cdb42850fd3288e35f947809a
vanessaruiva404-rgb <vanessaruiva404@gmail.com>
2026-06-29 23:33:00 -0400

    Update update.cs

--- update.cs
+++ update.cs
@@ -978,7 +978,7 @@ namespace LineageII
 
         private async Task<Manifest> FetchManifest()
         {
-            string manifestUrl = "http://157.254.248.55:8080/L2UpdaterWeb/api/manifest.php?t=" + DateTime.UtcNow.Ticks;
+            string manifestUrl = "http://157.254.248.55:8090/L2UpdaterWeb/api/manifest.php?t=" + DateTime.UtcNow.Ticks;
             string json = await client.GetStringAsync(manifestUrl);
             Manifest manifest = JsonConvert.DeserializeObject<Manifest>(json);
 
